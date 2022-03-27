a02d71df8ff20aa76bb42702034c486f58989785
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-03-26 20:03:40 -0600

    Beta

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,9 +1,208 @@
+//Nathan Hafley
+//Food Truck
+
 package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+import java.util.Scanner;
 
 public class FoodTruckApp {
 
+	// make array of objects
+	private FoodTruck[] trucks = new FoodTruck[5];
+	// The array is called trucks
+	// Food truck is the object
+	// Trucks is the array with 5 slots
+
+	/// ---------Main Method------------
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Make scanner
+		java.util.Scanner input = new Scanner(System.in);
+
+		// Created an instance of this class
+		// Makes non static reference
+		FoodTruckApp app = new FoodTruckApp();
+		app.run();
+
+		// Keeps program running
+		// terminates when user quits
+		boolean runMenu = true;
+		while (runMenu == true) {
+			// prints menu
+			app.printMenu();
+			System.out.println("Please enter A, B, C, or Q");
+			char menuSelection = input.next().toUpperCase().charAt(0);
+
+			// Switch to control menu input
+			switch (menuSelection) {
+
+			case 'A':
+				app.listFoodTrucks();
+				break;
+			case 'B':
+				app.averageRating();
+				break;
+			case 'C':
+				app.highestRating();
+				break;
+			case 'Q':
+				System.out.println("Goodbye");
+				input.close();
+				runMenu = false;
+				break;
+			default:
+				System.out.println("Invalid Option");
+
+			}
+
+		}
+
+	}
+
+//Methods
+//Used to access non static refrences 
+	private void run() {
+
+		// create scanner
+		java.util.Scanner input = new Scanner(System.in);
+
+		// FOR LOOP THAT COLLECTS USER INPUT
+		// iterates for each spot in the array
+		for (int i = 0; i < 5; i++) {
+
+			// Get name of food truck from user
+			System.out.println("Please Enter Name of Food Truck, or Enter Q to quit.");
+			String name = input.nextLine();
+
+			// Quits the program
+			if (name.toUpperCase().equals("Q")) {
+				input.close();
+				break;
+			} else {
+
+				// Get type of food truck from user
+				System.out.println("Please Enter Type of Food Truck.");
+				String foodType = input.nextLine();
+				// trucks[numFoodTrucks].setFoodTruckFoodType(input.nextLine());
+
+				// get user rating for for truck
+				System.out.println("Please Enter Rating for Food Truck.");
+				double rating = input.nextDouble();
+				input.nextLine();
+
+				// Call constructor to assign fields to truck
+				FoodTruck truck = new FoodTruck(name, foodType, rating);
+
+				// assign truck to location in trucks array
+				trucks[i] = truck;
+				// truck is the name of the object
+				// food truck is the datatype/object type
+				// trucks is the array
+
+			}
+		}
+
+	}
+
+	// --------DISPLAY MENU--------
+
+	private void printMenu() {
+
+		System.out.println();
+		System.out.println("============== MENU =============");
+		System.out.println("|                               |");
+		System.out.println("|  Enter A List FoodTrucks      |");
+		System.out.println("|  Enter B List Average Rating  |");
+		System.out.println("|  Enter C List Highest Rating  |");
+		System.out.println("|  Enter Q Exit                 |");
+		System.out.println("|                               |");
+		System.out.println("=================================");
+		System.out.println();
+
+	}
+
+	// ----------DISPLAY FOOD TRUCKS----------
+
+	private void listFoodTrucks() {
+		System.out.println("--------------------");
+		System.out.println("---DISPLAY TRUCKS---");
+		System.out.println("--------------------");
+
+		// loop to search Trucks array
+		// outputs Trucks with toString
+		for (int i = 0; i < 5; i++) {
+
+			// terminates if loop reaches Trucks array with null value
+			if (trucks[i] == null) {
+				break;
+
+			} else {
+				trucks[i].displayFoodTrucks();
+			}
+		}
+		System.out.println("");
+	}
+
+	// -------------find averageRating----------
+	private void averageRating() {
+		double rating;
+		double totalRating = 0;
+		double averageRating = 0;
+		int counter = 0;
+		System.out.println("--------------------");
+		System.out.println("---AVERAGE RATING---");
+		System.out.println("--------------------");
+
+		// loop to search Trucks array
+		// finds average
+		for (int i = 0; i < 5; i++) {
+
+			// terminates is loop reaches Trucks array with null value
+			if (trucks[i] == null) {
+				break;
+			} else {
+
+				rating = trucks[i].getFoodTruckRating();
+				totalRating = totalRating + rating;
+				counter++;
+
+			}
+		}
+		averageRating = totalRating / counter;
+		System.out.println("Average Rating: " + averageRating);
+		System.out.println("");
+
+	}
+
+	// -------------find highestRating---------
+	private void highestRating() {
+		double highestRating = 0;
+		double currentRating = 0;
+		double newRating = 0;
+		String bestTruck = new String();
+		System.out.println("---------------------");
+		System.out.println("---BEST FOOD TRUCK---");
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+
+			if (trucks[i] == null) {
+				break;
+			} else {
+
+				newRating = trucks[i].getFoodTruckRating();
+
+				if (currentRating < newRating) {
+					currentRating = newRating;
+					bestTruck = trucks[i].toString();
+				}
+
+			}
+		}
+		highestRating = currentRating;
+		System.out.println("Best Truck Award Goes to:");
+		System.out.println(bestTruck);
+		System.out.println("");
 
 	}
 
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,81 @@
+//Nathan Hafley
+//Food Truck
+
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+
+	// Field Variables
+	private String foodTruckName;
+	private String foodTruckFoodType;
+	private double foodTruckRating;
+	private int foodTruckID;
+	private static int nextFoodTruckID = 1000;
+
+	// ------------------------CONSTRUCTORS----------------------
+	// DEFAULT CONTRUCTOR
+	// TODO: Default constructor??
+	// TODO:DOES IT NEED ANYTHING??
+
+	// DEFAULT CONSTRUCTOR
+	public FoodTruck() {
+		// super();
+	}
+
+	// MAIN CONTRUCTORS
+	// OVERLOADED
+	// STAYS PUBLIC
+	public FoodTruck(String foodTruckName, String foodTruckFoodType, double foodTruckRating) {
+		// super();
+		this.foodTruckName = foodTruckName;
+		this.foodTruckFoodType = foodTruckFoodType;
+		this.foodTruckRating = foodTruckRating;
+		nextFoodTruckID = nextFoodTruckID + 1;
+		this.foodTruckID = nextFoodTruckID;
+
+	}
+
+	// ------------------ METHODS----------------------
+	// GETTERS AND SETTERS
+
+	public String getFoodTruckName() {
+		return foodTruckName;
+	}
+
+	public void setFoodTruckName(String foodTruckName) {
+		this.foodTruckName = foodTruckName;
+	}
+
+	public String getFoodTruckFoodType() {
+		return foodTruckFoodType;
+	}
+
+	public void setFoodTruckFoodType(String foodTruckFoodType) {
+		this.foodTruckFoodType = foodTruckFoodType;
+	}
+
+	public double getFoodTruckRating() {
+		return foodTruckRating;
+	}
+
+	public void setFoodTruckRating(double foodTruckRating) {
+		this.foodTruckRating = foodTruckRating;
+	}
+
+	// ----------------TOSTRINGS----------------
+
+	// TODO: Create TOSTRING
+	// TO STRING
+	// OUTPUT INFO ON FOOD TRUCK OBJECT
+	public String toString() {
+		return "Food Truck: " + foodTruckName + ", Food Type: " + foodTruckFoodType + ", Food Truck Rating: "
+				+ foodTruckRating + " Food Truck ID " + foodTruckID;
+	}
+
+	public void displayFoodTrucks() {
+		String foodTruckData = toString();
+		System.out.println(foodTruckData);
+		System.out.println("");
+	}
 
 }
